2fd30f811fb5366ed8c006464f65e0e096a59878
torch <harall09@evergreen.edu>
2020-02-02 00:26:35 -0500

    Updates

---
+++ precache-manifest.edd5fcc4ea0de68e98bd971f1b447b55.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "371ab4d0b02779c59f6d02b3be5ed61f",
+    "url": "/bloomingpixels-challenge/index.html"
+  },
+  {
+    "revision": "4558c7376f5840297d7a",
+    "url": "/bloomingpixels-challenge/static/css/main.dd9c7fa6.chunk.css"
+  },
+  {
+    "revision": "65f5c686c0bd6adb560a",
+    "url": "/bloomingpixels-challenge/static/js/2.8bd0602f.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/bloomingpixels-challenge/static/js/2.8bd0602f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4558c7376f5840297d7a",
+    "url": "/bloomingpixels-challenge/static/js/main.14cff2cc.chunk.js"
+  },
+  {
+    "revision": "16024e2145e297db4bf0",
+    "url": "/bloomingpixels-challenge/static/js/runtime-main.097ce6f0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bloomingpixels-challenge/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "52855b4091d31e98b61780ae7d4d6e20",
+    "url": "/bloomingpixels-challenge/static/media/magnify-light.52855b40.svg"
+  },
+  {
+    "revision": "190dff74aa5166c5dddd3a6689964c70",
+    "url": "/bloomingpixels-challenge/static/media/magnify.190dff74.svg"
+  }
+]);
