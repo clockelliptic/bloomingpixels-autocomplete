08ae3cbd6d46c3c37fd08b801a48536a9130c4f9
torch <harall09@evergreen.edu>
2020-02-03 09:39:18 -0500

    fixed broken fonts on apple devices

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import logo from './logo.svg'
 export default function App() {
   return (
     <>
-    <Slider orientation={'y'} navDotPos={'left'} transDuration={0.8} navDotColor={'rgba(100, 100, 100, 1)'}>
+    <Slider orientation={'x'} navDotPos={'bottom'} transDuration={0.8} navDotColor={'rgba(100, 100, 100, 1)'}>
       <Slide>
         <div
           style={{
@@ -23,7 +23,9 @@ export default function App() {
           <div className="message-alert">
             Please scroll or swipe to navigate examples.
           </div>
+
           <div className="logo-box" />
+
           <div style={{flex: 1, width: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', alignContent: 'center', alignItems: 'center'}}>
             <Autocomplete themeClass={"light"} />
           </div>
